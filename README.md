95e3af17decd7d8e8ab1b7f097612a1008032104
David House <davidahouse@gmail.com>
2022-10-10 08:19:58 -0400

    :tada: Initial commmit

---
+++ README.md
@@ -0,0 +1,6 @@
+# XCTestExamples
+
+This repository is an Xcode project used to test some of the tools based on XCResultKit. It includes as many different 
+examples of things you can do in tests as possible.
+
+
---
+++ XCTestExamples/XCTestExamples/XCTestExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestExamplesApp.swift
+//  XCTestExamples
+//
+//  Created by David House on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct XCTestExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
